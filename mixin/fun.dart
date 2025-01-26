e449361e5328b654d60f38ffd34427a747eb7873
Vipul sharma <vipulsharma6021@gmail.com>
2025-01-26 23:42:25 +0530

    Add user, student, and teacher classes with mixin implementation

---
+++ mixin/fun.dart
@@ -0,0 +1,31 @@
+
+abstract class user{
+  late String name;
+  late int age;
+  user({required this.name, required this.age});
+}
+
+mixin prouser{
+  late int salary;
+  void display(){
+    print('hello vipul');
+  }
+}
+
+class student extends user{
+  student({required String name, required int age}): super(name: name, age: age);
+
+  void sayname(){
+    print('${this.name} and ${this.age}');
+  }
+}
+
+class primeryteacher extends user with prouser{
+  primeryteacher({required String name, required int age}): super(name: name, age: age);
+  
+  
+}
+
+class highteacher extends user with prouser{
+  highteacher({required String name, required int age}): super(name: name, age: age);
+}
---
+++ mixin/main.dart
@@ -0,0 +1,8 @@
+import 'fun.dart';
+void main(){
+  highteacher Highteacher = new highteacher(name: 'vipul', age: 21);
+  print(Highteacher.name);
+  
+  var call = student(name: 'vikash', age: 1);
+  print(call);
+}
