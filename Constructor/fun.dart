67bd6504cbebff12ea135da6251bff2a6a26ba6f
Vipul sharma <vipulsharma6021@gmail.com>
2025-02-11 21:21:53 +0530

    Implement Singleton pattern in Name class to ensure a single instance is created

--- Constructor/fun.dart
+++ Constructor/fun.dart
@@ -1,6 +1,17 @@
 class Name{
-  late String name;
-  late int age;
+  static Name? _name; //why static? because we want to access it without creating instance of class;
 
-  Name(this.name, this.age);
+  Name._();
+
+  factory Name(){
+    _name ??= Name._(); // ?? means if _name is null then create new instance;
+    return _name!; // ! means it will not be null;
+  }
+}
+
+void main(){
+  var s = Name();
+  var s1 = Name();
+
+  print(s == s1);
 }
